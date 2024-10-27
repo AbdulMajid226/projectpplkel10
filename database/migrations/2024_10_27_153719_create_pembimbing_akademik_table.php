0b449f08efe539ff93de7d1f7626cf63b3696394
AbdulMajid226 <abdulmajid30092003@gmail.com>
2024-10-27 22:44:32 +0700

    inisialisasi database

---
+++ database/migrations/2024_10_27_153719_create_pembimbing_akademik_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembimbing_akademik', function (Blueprint $table) {
+            $table->id('id_pembimbing');
+            $table->unsignedBigInteger('id_mahasiswa');
+            $table->unsignedBigInteger('id_dosen');
+            $table->timestamps();
+
+            $table->foreign('id_mahasiswa')->references('id_mahasiswa')->on('mahasiswa');
+            $table->foreign('id_dosen')->references('id_dosen')->on('dosen');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembimbing_akademik');
+    }
+};
