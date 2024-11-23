d3d35b306a409768737047b07398395d058b4396
AbdulMajid226 <abdulmajid30092003@gmail.com>
2024-11-23 18:44:33 +0700

    menerapkan update navbar dan sidebar di semua actor

--- resources/views/dashboard_dekan.blade.php
+++ resources/views/dashboard_dekan.blade.php
@@ -6,63 +6,96 @@
 </x-header>
 
 <body class="bg-gray-100">
+    <!--Navbar  -->
+    <x-nav-bar>
+        <x-slot:name>Mulyono</x-slot:name>
+        <x-slot:role>Dekan</x-slot:role>
+    </x-nav-bar>
 
-    <div class="flex h-screen">
+    <!--SideBar  -->
+    <x-side-bar>
+        <x-slot:items>
+            <li>
+                <a href="/dashboard_dekan"
+                    class="flex items-center p-2 text-gray-100  rounded-lg hover:bg-teal-700 group">
+                    <!-- Logo dashboard -->
+                    <svg class="w-6 h-6 text-gray-400 transition duration-75 group-hover:text-white " aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                        <path
+                            d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                        <path
+                            d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                    </svg>
+                    <span class="ms-3">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="" class="flex items-center p-2 text-gray-100  rounded-lg hover:bg-teal-700 group">
+                    <!-- Logo Ajukan Ruang Kuliah -->
+                    <svg class="w-6 h-6 text-gray-400 transition duration-75 group-hover:text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M18 18V6h-5v12h5Zm0 0h2M4 18h2.5m3.5-5.5V12M6 6l7-2v16l-7-2V6Z" />
+                    </svg>
 
-        <x-nav-bar>
-            <x-slot:items>
-                <x-nav-link href="/dashboard_dekan" :active="request()->is('dashboard_dekan')">
-                    Dashboard
-                </x-nav-link>
-                <x-nav-link href="" :active="request()->is('')">Pengesahan Ruang Kuliah</x-nav-link>
-                <x-nav-link href="" :active="request()->is('')">Pengesahan Jadwal Kuliah</x-nav-link>
+                    <span class="ms-3">Pengesahan Ruang Kuliah</span>
+                </a>
+            </li>
+            <li>
+                <a href="" class="flex items-center p-2 text-gray-100  rounded-lg hover:bg-teal-700 group">
+                    <!-- Logo Kelola Ruang Kuliah -->
+                    <svg class="w-6 h-6 text-gray-400 group-hover:text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" />
+                    </svg>
 
-            </x-slot:items>
-            <x-slot:name>Mulyono</x-slot:name>
-            <x-slot:role>Dekan</x-slot:role>
-        </x-nav-bar>
+                    <span class="ms-3">Pengesahan Jadwal Kuliah</span>
+                </a>
+            </li>
+        </x-slot:items>
+    </x-side-bar>
 
-        <!-- Main Content -->
-        <main class="flex-1 p-8 space-y-6">
-            <!-- Informasi Pengesahan Ruang Kuliah -->
+    <!-- Main Content -->
+    <main class="flex-1 p-8 space-y-6 sm:ml-2 md:ml-60 lg:ml-60 mt-10">
+        <!-- Informasi Pengesahan Ruang Kuliah -->
 
-            <h1 class="text-lg font-bold">Informasi Pengesahan Ruang Kuliah</h1>
+        <h1 class="text-lg font-bold">Informasi Pengesahan Ruang Kuliah</h1>
 
-            <div class="grid grid-cols-1 gap-4 p-3 bg-teal-600 rounded-lg shadow md:grid-cols-3">
-                <div class="p-4 text-center bg-white rounded-lg shadow">
-                    <p class="text-2xl font-bold">Sudah Disetujui</p>
-                    <p>20</p>
-                </div>
-                <div class="p-4 text-center bg-white rounded-lg shadow">
-                    <p class="text-2xl font-bold">Belum Disetujui</p>
-                    <p>20</p>
-                </div>
-                <div class="p-4 text-center bg-white rounded-lg shadow">
-                    <p class="text-2xl font-bold">Ditolak</p>
-                    <p>10</p>
-                </div>
+        <div class="grid grid-cols-1 gap-4 p-3 bg-teal-600 rounded-lg shadow md:grid-cols-3">
+            <div class="p-4 text-center bg-white rounded-lg shadow">
+                <p class="text-2xl font-bold">Sudah Disetujui</p>
+                <p>20</p>
             </div>
-
-            <!-- Informasi Pengesahan Jadwal Kuliah -->
-
-            <h1 class="text-lg font-bold">Informasi Pengesahan Jadwal Kuliah</h1>
-
-            <div class="grid grid-cols-1 gap-4 p-3 bg-teal-600 rounded-lg shadow md:grid-cols-3">
-                <div class="p-4 text-center bg-white rounded-lg shadow">
-                    <p class="text-2xl font-bold">Sudah Disetujui</p>
-                    <p>20</p>
-                </div>
-                <div class="p-4 text-center bg-white rounded-lg shadow">
-                    <p class="text-2xl font-bold">Belum Disetujui</p>
-                    <p>20</p>
-                </div>
-                <div class="p-4 text-center bg-white rounded-lg shadow">
-                    <p class="text-2xl font-bold">Ditolak</p>
-                    <p>10</p>
-                </div>
+            <div class="p-4 text-center bg-white rounded-lg shadow">
+                <p class="text-2xl font-bold">Belum Disetujui</p>
+                <p>20</p>
             </div>
-        </main>
-    </div>
+            <div class="p-4 text-center bg-white rounded-lg shadow">
+                <p class="text-2xl font-bold">Ditolak</p>
+                <p>10</p>
+            </div>
+        </div>
+
+        <!-- Informasi Pengesahan Jadwal Kuliah -->
+
+        <h1 class="text-lg font-bold">Informasi Pengesahan Jadwal Kuliah</h1>
+
+        <div class="grid grid-cols-1 gap-4 p-3 bg-teal-600 rounded-lg shadow md:grid-cols-3">
+            <div class="p-4 text-center bg-white rounded-lg shadow">
+                <p class="text-2xl font-bold">Sudah Disetujui</p>
+                <p>20</p>
+            </div>
+            <div class="p-4 text-center bg-white rounded-lg shadow">
+                <p class="text-2xl font-bold">Belum Disetujui</p>
+                <p>20</p>
+            </div>
+            <div class="p-4 text-center bg-white rounded-lg shadow">
+                <p class="text-2xl font-bold">Ditolak</p>
+                <p>10</p>
+            </div>
+        </div>
+    </main>
 
 
 </body>
--- resources/views/dashboard_kaprodi.blade.php
+++ resources/views/dashboard_kaprodi.blade.php
@@ -6,50 +6,74 @@
 </x-header>
 
 <body class="bg-gray-100">
+    <!--Navbar  -->
+    <x-nav-bar>
+        <x-slot:name>Mulyono</x-slot:name>
+        <x-slot:role>Kaprodi</x-slot:role>
+    </x-nav-bar>
 
-    <div class="flex h-screen">
+    <!--SideBar  -->
+    <x-side-bar>
+        <x-slot:items>
+            <li>
+                <a href="/dashboard_kaprodi"
+                    class="flex items-center p-2 text-gray-100  rounded-lg hover:bg-teal-700 group">
+                    <!-- Logo dashboard -->
+                    <svg class="w-6 h-6 text-gray-400 transition duration-75 group-hover:text-white " aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                        <path
+                            d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                        <path
+                            d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                    </svg>
+                    <span class="ms-3">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="" class="flex items-center p-2 text-gray-100  rounded-lg hover:bg-teal-700 group">
+                    <!-- Logo Ajukan Ruang Kuliah -->
+                    <svg class="w-6 h-6 text-gray-400 group-hover:text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" />
+                    </svg>
 
-        <x-nav-bar>
-            <x-slot:items>
-                <x-nav-link href="/dashboard_kaprodi" :active="request()->is('dashboard_kaprodi')">
-                    Dashboard
-                </x-nav-link>
-                <x-nav-link href="" :active="request()->is('')">Buat Jadwal Kuliah</x-nav-link>
+
+                    <span class="ms-3">Buat Jadwal Kuliah</span>
+                </a>
+            </li>
+        </x-slot:items>
+    </x-side-bar>
 
 
-            </x-slot:items>
-            <x-slot:name>Mulyono</x-slot:name>
-            <x-slot:role>Kaprodi</x-slot:role>
-        </x-nav-bar>
+    <!-- Main Content -->
+    <main class="flex-1 p-8 space-y-6 sm:ml-2 md:ml-60 lg:ml-60 mt-10">
+        <!-- Informasi Jadwal Kuliah -->
 
-        <!-- Main Content -->
-        <main class="flex-1 p-8 space-y-6">
-            <!-- Informasi Jadwal Kuliah -->
-
-            <h1 class="text-lg font-bold">Informasi Jadwal Kuliah</h1>
-            <h1 class="text-lg font-semibold">Semester Ganjil 2024/2025</h1>
-            <div class="grid grid-cols-1 gap-4 p-3 bg-teal-600 rounded-lg shadow md:grid-cols-3">
-                <div class="p-4 text-center bg-white rounded-lg shadow">
-                    <p class="text-2xl font-bold">Sudah Disetujui</p>
-                    <p>20</p>
-                </div>
-                <div class="p-4 text-center bg-white rounded-lg shadow">
-                    <p class="text-2xl font-bold">Menunggu Persetujuan</p>
-                    <p>20</p>
-                </div>
-                <div class="p-4 text-center bg-white rounded-lg shadow">
-                    <p class="text-2xl font-bold">Ditolak</p>
-                    <p>10</p>
-                </div>
-                <!-- <div class="p-4 text-center bg-white rounded-lg shadow">
+        <h1 class="text-lg font-bold">Informasi Jadwal Kuliah</h1>
+        <h1 class="text-lg font-semibold">Semester Ganjil 2024/2025</h1>
+        <div class="grid grid-cols-1 gap-4 p-3 bg-teal-600 rounded-lg shadow md:grid-cols-3">
+            <div class="p-4 text-center bg-white rounded-lg shadow">
+                <p class="text-2xl font-bold">Sudah Disetujui</p>
+                <p>20</p>
+            </div>
+            <div class="p-4 text-center bg-white rounded-lg shadow">
+                <p class="text-2xl font-bold">Menunggu Persetujuan</p>
+                <p>20</p>
+            </div>
+            <div class="p-4 text-center bg-white rounded-lg shadow">
+                <p class="text-2xl font-bold">Ditolak</p>
+                <p>10</p>
+            </div>
+            <!-- <div class="p-4 text-center bg-white rounded-lg shadow">
                     <p class="text-2xl font-bold">Belum diajukan</p>
                     <p>10</p>
                 </div> -->
-            </div>
+        </div>
 
 
-        </main>
-    </div>
+    </main>
+
 
 
 </body>
